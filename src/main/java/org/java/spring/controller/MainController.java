4c8ed999498576aa9877ee810112dac31ca119d4
Patrickhell <patrickhell19@yahoo.fr>
2023-12-04 14:44:48 +0100

    Added dependency validation, form to create a new pizza with validations

--- src/main/java/org/java/spring/controller/MainController.java
+++ src/main/java/org/java/spring/controller/MainController.java
@@ -3,14 +3,20 @@ package org.java.spring.controller;
 
 import java.util.List;
 
+
 import org.java.spring.db.pojo.Pizza;
 import org.java.spring.db.service.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class MainController {
@@ -43,6 +49,36 @@ public class MainController {
 		
 		
 	}
+	
+	@GetMapping("/pizze/create")
+	public String create(Model model) {
+		
+		model.addAttribute("pizza", new Pizza());
+		
+		return"/pizza-form";
+	}
+	
+	@PostMapping("/pizze/create")
+	public String pizzaStore(Model model, @Valid @ModelAttribute Pizza pizza, BindingResult bindingResult) {
+		
+
+		System.out.println("Book:\n" + pizza);
+		System.out.println("\n---------------\n");
+		System.out.println("Error:\n" + bindingResult);
+		
+		if(bindingResult.hasErrors()) {
+			
+			System.out.println(bindingResult);
+			model.addAttribute("pizza", pizza);
+			return "/pizza-form";
+		}
+		
+		pizzaService.save(pizza);
+		
+		return "redirect:/";
+	}
+	
+	
 	
 	
 	
--- src/main/java/org/java/spring/db/pojo/Pizza.java
+++ src/main/java/org/java/spring/db/pojo/Pizza.java
@@ -1,11 +1,18 @@
 package org.java.spring.db.pojo;
 
 
+import org.hibernate.validator.constraints.Length;
+
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Pizza {
@@ -15,15 +22,23 @@ public class Pizza {
 	private int id;
 	
 	@Column(nullable = false, unique = true)
+	@Length (min = 5, max = 60, message = "Il nome aver un numero di caratteri compreso tra 5 e 60")
+	@NotEmpty(message = "Il campo nome non può essere vuoto")
 	private String name;
 	
 	@Column(columnDefinition = "TEXT")
+	@NotEmpty(message = "Il campo descrizione non può essere vuoto")
+	@Length (min = 10 , max = 120, message = "Il nome aver un numero di caratteri compreso tra 10 e 120")
 	private String description;
 	
 	@Column(length = 255)
+	@NotEmpty(message = "Il image Url nome non può essere vuoto")
 	private String foto;
 	
 	@Column(length = 60)
+	@NotNull(message = "Il campo prezzo non può essere vuoto")
+	@DecimalMin(value = "0.0" , inclusive = false, message = "Il prezzo deve essere maggiore di 0 ")
+	@DecimalMax(value = "15.0" , inclusive = true, message = "Il prezzo non può essere maggiore di 15 euro ")
 	private double price;
 	
 	public Pizza() { }
